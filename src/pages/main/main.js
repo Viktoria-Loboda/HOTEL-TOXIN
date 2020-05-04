e9b15cb9199c2fedb1c2eda5df132496cce385cf
Vika <MishaPandovich@yandex.ru>
2020-05-04 21:10:13 +0300

    сделан слайд на главной странице

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,1 +1,24 @@
 import './main.scss';
+import DatePicker from '../../components/date-picker/date-picker.js';
+import Dropdown from '../../components/dropdown/dropdown.js';
+
+$('#date-picker').datepicker({
+	range: true,
+	inline: false,
+	clearButton: true,
+	disableNavWhenOutOfRange: false,
+	minDate: new Date()
+});
+
+let myDatepicker = $('#date-picker').datepicker().data('datepicker');
+let datepicker = new DatePicker('wrapper-date-picker', myDatepicker);
+
+
+let dropdownWords = {
+  row1: ['взрослый', 'взрослых', 'взрослых'],
+  row2: ['ребенок', 'детей', 'детей'],
+  row3: ['младенец', 'младенцев', 'младенцев'],
+  phrase: 'Сколько гостей'
+}
+
+let newDropdown = new Dropdown('wrapper-dropdown', dropdownWords);
